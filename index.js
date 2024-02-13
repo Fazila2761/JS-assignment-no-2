2e489c5049332a1446d4e49ef9ac5648cad5287f
Fazila2761 <fazilakhan2761@gmail.com>
2024-02-13 14:52:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+{let x = 5;
+x++;
+let z = x;
+console.log(z)
+}
+// ----------------------------defined console log------------------
+{
+var num1=5;
+num2=5;
+total=num1*num2;
+console.log(total);
+}
+// ----------------------------------------------------------
+{
+    var firstname="Atia";
+       lastname="khan";
+       fullname=firstname+" "+lastname
+        console.log(fullname)
+       
+}
+
+
